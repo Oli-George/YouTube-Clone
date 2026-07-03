b2dd9f0cead1d081c7bf3aa1154271fdbb87d33f
George <olisakwegeorgejunior@gmail.com>
2026-07-03 21:56:25 +0100

    My "holiday" is coming to an end

--- YouTube Clone/index.js
+++ YouTube Clone/index.js
@@ -3,4 +3,5 @@ const n = 'MTH 206';
 function print_name {
     const name = prompt("What's your name, dear user?");
     console.log(`Happy to have you here, ${name}!`);
+//For now, it is completely unimportant. 
 }
