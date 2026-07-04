3a13a2db7c96d6c00b25c7c4ae4b78303ea85fdd
George Olisakwe <olisakwegeorgejunior@gmail.com>
2026-07-04 21:37:36 +0100

    Got a reading at mass tomorrow.

--- YouTube Clone/index.js
+++ YouTube Clone/index.js
@@ -3,6 +3,8 @@ const n = 'MTH 206';
 function print_name() {
     const name = prompt("What's your name, dear user?");
     console.log(`Happy to have you here, ${name}!`);
+    console.log(`How on earth did I get a B in ${n}? 😠😠`)
 //For now, it is completely unimportant. 
 }
+
 print_name();
