0a4885fa2947297cd0e3d6bca01140939a093572
George Olisakwe <olisakwegeorgejunior@gmail.com>
2026-07-25 22:08:14 +0100

    Switched it to an arrow function syntax.

--- YouTube Clone/index.js
+++ YouTube Clone/index.js
@@ -1,10 +1,10 @@
-const n = 'MTH 206';
+const n = "MTH 206";
 
-function print_name() {
-    const name = prompt("What's your name, dear user?");
-    console.log(`Happy to have you here, ${name}!`);
-    console.log(`How on earth did I get a B in ${n}? 😠😠`)
-//For now, it is completely unimportant. 
-}
+const print_name = () => {
+  const name = prompt("What's your name, dear user?");
+  console.log(`Happy to have you here, ${name}!`);
+  console.log(`How on Earth did I get a B in ${n}? 😠😠`);
+  //For now, it is completely unimportant.
+};
 
 print_name();
