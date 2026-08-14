74932badbae41e0ac118d4617bbe70f8fb5326d7
George <olisakwegeorgejunior@gmail.com>
2026-08-14 22:47:33 +0100

    Current feelings

--- YouTube Clone/index.js
+++ YouTube Clone/index.js
@@ -3,8 +3,10 @@ const n = "MTH 206";
 const print_name = () => {
   const name = prompt("What's your name, dear user?");
   console.log(`Happy to have you here, ${name}!`);
-  console.log(`How on Earth did I get a B in ${n}? 😠😠`);
+  console.log(`How on Earth did I get a B in ${n}? 😠😡`);
   //For now, it is completely unimportant.
 };
 
 print_name();
+
+// Man my ML journey has really taken a hit. I need to get back on track to catch up with Betty. 
