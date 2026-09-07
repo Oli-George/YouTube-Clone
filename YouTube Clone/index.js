43f26097b7392f4e34b807d2bc7a6e947c38c5bf
George Olisakwe <olisakwegeorgejunior@gmail.com>
2026-09-07 21:41:20 +0100

    Consultation for the project in mind

--- YouTube Clone/index.js
+++ YouTube Clone/index.js
@@ -1,7 +1,7 @@
 const n = "MTH 206";
 
 const print_name = () => {
-  const name = prompt("What's your name, dear user?");
+  const name = prompt("What's your name, querido user?");
   console.log(`Happy to have you here, ${name}!`);
   console.log(`How on Earth did I get a B in ${n}? 😠😡`);
   //For now, it is completely unimportant.
