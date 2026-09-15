a36f87f9215d9ad2df26e0e9a54c99345896a5cb
George Olisakwe <olisakwegeorgejunior@gmail.com>
2026-09-15 23:29:55 +0100

    Began my SIWES report today

--- YouTube Clone/index.js
+++ YouTube Clone/index.js
@@ -1,13 +1,21 @@
 const n = "MTH 206";
 
 const print_name = () => {
-  const name = prompt("What's your name, querido user?");
+  const name = prompt("What's your name, querido usante?");
   console.log(`Happy to have you here, ${name}!`);
   console.log(`How on Earth did I get a B in ${n}? 😠😡`);
+  console.log(
+    `${num} is a number that I will never use in my life. It is completely useless. I don't even know why I am writing this. I guess it is just to fill up space.`,
+  );
   //For now, it is completely unimportant.
 };
 
-print_name();
+function rien() {
+  let num = 35;
+  num++;
+}
+
+print_name(rien());
 
 window.open("www.wikipedia.org", "Test-stuff", "width=500, height=500");
 // Man my ML journey has really taken a hit. I need to get back on track to catch up with Betty.
